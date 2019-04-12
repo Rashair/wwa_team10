ce5e84471d282bd1bcb8773e02ce051f57aacf93
maciekwnuk <macekw1994@wp.pl>
2019-04-12 23:01:13 +0200

    Add template functions with Spring

--- backend/src/main/java/pl/allegro/braincode/team10/dto/PointBasic.java
+++ backend/src/main/java/pl/allegro/braincode/team10/dto/PointBasic.java
@@ -1,4 +1,18 @@
 package pl.allegro.braincode.team10.dto;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class PointBasic {
+    @GetMapping(value = "/client/")
+    @ResponseBody
+    public String apiGet() {
+
+    }
+
+    @PutMapping(value = "/client/address")
+    @ResponseBody
+    public String apiPut() {
+
+    }
 }
