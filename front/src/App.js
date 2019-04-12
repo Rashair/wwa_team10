0739ef5cfcb1280f38d2f9b7c0a881be815eb58a
Jan Barszcz <jan@MacBook-Pro-Jan.local>
2019-04-12 21:03:56 +0200

    added map component

--- front/src/App.js
+++ front/src/App.js
@@ -1,37 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+import Map from './components/Map.js'
 
-class NameForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {value: ''};
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-
-  handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
-    event.preventDefault();
-  }
+class App extends React.PureComponent {
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Name:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-    );
+      <div>
+       <Map/>
+      </div>
+      
+      
+    )
   }
 }
 
 
-export default NameForm;
+export default App;
