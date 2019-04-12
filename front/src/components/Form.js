7c8732aeffeff83bee8723a10261658573faf4e1
Przemysław Wiczołek <przemyslaw.wk@gmail.com>
2019-04-12 23:42:55 +0200

    Additional updates to form

--- front/src/components/Form.js
+++ front/src/components/Form.js
@@ -5,16 +5,28 @@ class DeliveryForm extends React.Component {
     constructor(props) {
         super(props);
 
-        this.street = { value: 'Jana Pawła II' };
-        this.houseNumber = { value: '22/13' };
-        this.postalCode = { value: '00-133' };
-        this.city = { value: 'Warszawa' };
-        this.phoneNumber = { value: '+48 516 942 392' };
+        this.address = {
+            street: 'Jana Pawła II',
+            houseNumber: '22/13' ,
+            postalCode : '00-133' ,
+            city: 'Warszawa',
+            phoneNumber: '+48 516 942 392' 
+        };
 
-        this.range = {value: '1000'};
-        this.hoursFrom = {value: '08:00'};
-        this.hoursTo = {value: '16:00'};
-        this.parkingChecked = {value: 'false'}
+        this.deliveryPoint = {
+            name: 'WAW22',
+            hoursFrom: '08:00',
+            hoursTo: '16:00',
+            address: 'Jana Pawła II 22/419 Warszawa'
+        };
+
+        this.deliveryPointParameters = {
+            range: '1000',
+            hoursFrom: '08:00',
+            hoursTo:  '16:00',
+            parkingChecked: 'false'
+        };
+
 
         this.handleChange = this.handleChange.bind(this);
     }
@@ -29,42 +41,44 @@ class DeliveryForm extends React.Component {
             <form onSubmit={this.handleSubmit}>
                 <fieldset  className="dataBlock" id="address">
                     <legend> Adres </legend>
-                    <div> {this.street.value} </div>
-                    <div> {this.houseNumber.value} </div>
-                    <div> {this.postalCode.value} </div>
-                    <div> {this.city.value} </div>
-                    <div> {this.phoneNumber.value} </div>
+                    <div> {this.address.street} </div>
+                    <div> {this.address.houseNumber} </div>
+                    <div> {this.address.postalCode} </div>
+                    <div> {this.address.city} </div>
+                    <div> {this.address.phoneNumber} </div>
                 </fieldset>
 
                 <fieldset className="dataBlock" id="defaultDeliveryPoint">
-                    
+                    <legend> Punkt odbioru </legend>
+                    <div>  </div>
+   
                 </fieldset>
 
                 <fieldset  className="dataBlock" id="deliveryPoints">
-
+                    <legend>Proponowane punkty odbioru</legend>
                 </fieldset>
 
-                <fieldset  className="dataBlock" id="deliveryParameters">
+                <fieldset  className="dataBlock" id="deliveryPointParameters">
                     <legend>Parametry punktów odbioru</legend>
 
                     <fieldset className="formRow">
                         <label htmlFor="range">Odległość[m]</label>
-                        <input id="range" type="number" defaultValue={this.range.value} />
+                        <input id="range" type="number" defaultValue={this.deliveryPointParameters.range} />
                     </fieldset>
                     <fieldset className="formRow">
                         <legend>Godziny otwarcia</legend>
                         <fieldset className="formRow">
                             <label htmlFor="hoursFrom"> Od: </label>
-                            <input id="hoursFrom" type="time" defaultValue={this.hoursFrom.value} />
+                            <input id="hoursFrom" type="time" defaultValue={this.deliveryPointParameters.hoursFrom} />
                         </fieldset>
                         <fieldset className="formRow">
                             <label htmlFor="hoursTo"> Do: </label>
-                            <input id="hoursTo" type="time" defaultValue={this.hoursTo.value} />
+                            <input id="hoursTo" type="time" defaultValue={this.deliveryPointParameters.hoursTo} />
                         </fieldset>
                     </fieldset>
                     <fieldset className="formRow">
                         <label htmlFor="parking"> Parking </label>
-                        <input id="parking" type="checkbox" defaultChecked={this.parkingChecked.value}></input>
+                        <input id="parking" type="checkbox" defaultChecked={this.deliveryPointParameters.parkingChecked}></input>
                     </fieldset>
 
                 </fieldset>
