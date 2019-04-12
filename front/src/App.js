968975e84f1128ee9c87b910e3b6a7d8e56f4d50
Jan Barszcz <jan@MacBook-Pro-Jan.local>
2019-04-12 22:45:56 +0200

    added markers

--- front/src/App.js
+++ front/src/App.js
@@ -5,13 +5,21 @@ import Map from './components/Map.js'
 
 class App extends React.PureComponent {
 
+
   render() {
+
+    const markersPositions = [
+      { lat: 52.2297, lng: 21.0122 },
+      { lat: 52, lng: 21 },
+    ]
+
+
     return (
       <div>
-       <Map/>
+        <Map markersPositions={markersPositions}/>
       </div>
-      
-      
+
+
     )
   }
 }
