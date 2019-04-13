eec4197b203ef81f433ae9f26d6f62a5f653d1c1
Przemysław Wiczołek <przemyslaw.wk@gmail.com>
2019-04-13 02:03:31 +0200

    Added address form

--- front/src/components/Table.js
+++ front/src/components/Table.js
@@ -5,14 +5,14 @@ class DeliveryPointsTable extends React.Component {
     render() {
 
         const deliveryPoints = [
-            {name: 'WAW22', hoursFrom: '08:00', hoursTo: '16:00', address: 'Jana Pawła II 22/419 Warszawa'},
+            {name: 'WAW22', hoursFrom: '08:00', hoursTo: '16:00', address: 'Jana Pawła II 22 Warszawa'},
             {name: 'WAW23', hoursFrom: '08:00', hoursTo: '16:00', address: 'XXXXXXXXXXXXXXXXXXXXX'},
             {name: 'WAW24', hoursFrom: '08:00', hoursTo: '16:00', address: 'YYYYYYYYYYYYYYYYYYYYYY'},
             {name: 'WAW25', hoursFrom: '21:00', hoursTo: '23:00', address: 'ZZZZZZZZZZZZZZZZ'}
         ];
 
         this.items = deliveryPoints.map((item, key) =>
-            <tr>
+            <tr key={item.name}>
                 <td>{item.name}</td>
                 <td>{item.hoursFrom} - {item.hoursTo}</td>
                 <td>{item.address}</td>
@@ -20,8 +20,8 @@ class DeliveryPointsTable extends React.Component {
         ) ;
 
         return (
-         <table id="deliveryPointsTable">
-             <tbody>
+         <table>
+             <tbody id="deliveryPointsTable">
                 {this.items}
              </tbody>
          </table>
