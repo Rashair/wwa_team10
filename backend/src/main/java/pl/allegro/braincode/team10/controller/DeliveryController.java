dfef8cc41830c63a0a5f8f991fc8b8fbe96732fd
kjedrzejczak <jedrek412@gmail.com>
2019-04-13 11:47:17 +0200

    Added coordinations of source address

--- backend/src/main/java/pl/allegro/braincode/team10/controller/DeliveryController.java
+++ backend/src/main/java/pl/allegro/braincode/team10/controller/DeliveryController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.allegro.braincode.team10.dto.DeliveryPoint;
-import pl.allegro.braincode.team10.dto.ListDTO;
+import pl.allegro.braincode.team10.dto.NearestDeliveryPointsDTO;
 import pl.allegro.braincode.team10.dto.SearchDeliveryPointDTO;
 import pl.allegro.braincode.team10.google.dtoGoogle.Coordinates;
 import pl.allegro.braincode.team10.google.service.query.GoogleQueryService;
@@ -15,6 +15,8 @@ import pl.allegro.braincode.team10.inPost.model.Location;
 import pl.allegro.braincode.team10.inPost.service.query.InPostQueryService;
 import pl.allegro.braincode.team10.mapper.CoordinatesMapper;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 @RestController
@@ -37,16 +39,18 @@ public class DeliveryController {
     }
 
     @PostMapping
-    public ResponseEntity<ListDTO<DeliveryPoint>> getDeliveryPoints(
+    public ResponseEntity<NearestDeliveryPointsDTO> getDeliveryPoints(
             @RequestBody(required = true) SearchDeliveryPointDTO searchDeliveryPointDTO) {
         //walidacja danych
         Coordinates coordinates = this.googleQueryService.getCoordinates(searchDeliveryPointDTO.getAddress());
         Location location = this.coordinatesMapper.coordinatesToLocation(coordinates);
         List<DeliveryPoint> deliveryPoints = this.inPostQueryService.getDeliveryPoints(searchDeliveryPointDTO, location);
         this.googleQueryService.fillDistanceToPoint(deliveryPoints, coordinates);
-        ListDTO<DeliveryPoint> listDTO = new ListDTO<>();
-        listDTO.setValues(deliveryPoints);
-        return new ResponseEntity<>(listDTO, HttpStatus.OK);
+        Collections.sort(deliveryPoints, Comparator.comparing(DeliveryPoint::getDistanceValue));
+        NearestDeliveryPointsDTO nearestDeliveryPointsDTO = new NearestDeliveryPointsDTO();
+        nearestDeliveryPointsDTO.setDeliveryPointList(deliveryPoints);
+        nearestDeliveryPointsDTO.setSourceLatitude(location.getLatitude());
+        nearestDeliveryPointsDTO.setSourceLongitude(location.getLongitude());
+        return new ResponseEntity<>(nearestDeliveryPointsDTO, HttpStatus.OK);
     }
-
 }
