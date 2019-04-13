3d845795d8922461e4ecc2dd9802c274681946f8
Przemysław Wiczołek <przemyslaw.wk@gmail.com>
2019-04-13 02:39:23 +0200

    Added default delivery point

--- front/src/components/Table.js
+++ front/src/components/Table.js
@@ -5,7 +5,7 @@ class DeliveryPointsTable extends React.Component {
     render() {
 
         const deliveryPoints = [
-            {name: 'WAW22', hoursFrom: '08:00', hoursTo: '16:00', address: 'Jana Pawła II 22 Warszawa'},
+            {name: 'WAW22', hoursFrom: '08:00', hoursTo: '16:00', address: 'Jana Pawła II 22/413 Warszawa'},
             {name: 'WAW23', hoursFrom: '08:00', hoursTo: '16:00', address: 'XXXXXXXXXXXXXXXXXXXXX'},
             {name: 'WAW24', hoursFrom: '08:00', hoursTo: '16:00', address: 'YYYYYYYYYYYYYYYYYYYYYY'},
             {name: 'WAW25', hoursFrom: '21:00', hoursTo: '23:00', address: 'ZZZZZZZZZZZZZZZZ'}
@@ -14,10 +14,10 @@ class DeliveryPointsTable extends React.Component {
         this.items = deliveryPoints.map((item, key) =>
             <tr key={item.name}>
                 <td>{item.name}</td>
-                <td>{item.hoursFrom} - {item.hoursTo}</td>
+                <td>{item.hoursFrom}-{item.hoursTo}</td>
                 <td>{item.address}</td>
             </tr>
-        ) ;
+        );
 
         return (
          <table>
@@ -26,7 +26,7 @@ class DeliveryPointsTable extends React.Component {
              </tbody>
          </table>
         )
-      }
+    }
 }
 
 export default DeliveryPointsTable;
