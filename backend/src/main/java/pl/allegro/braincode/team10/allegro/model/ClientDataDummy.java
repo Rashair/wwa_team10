b2e97e38917f95be1017392e62f0209445ed17a9
maciekwnuk <macekw1994@wp.pl>
2019-04-13 09:41:52 +0200

    Add building number and flat number to client dummy

--- backend/src/main/java/pl/allegro/braincode/team10/allegro/model/ClientDataDummy.java
+++ backend/src/main/java/pl/allegro/braincode/team10/allegro/model/ClientDataDummy.java
@@ -8,21 +8,22 @@ public class ClientDataDummy {
     private ArrayList<String> cities = new ArrayList<>();
     private ArrayList<String> streets = new ArrayList<>();
     private ArrayList<String> post_code = new ArrayList<>();
+    private ArrayList<String> building_number = new ArrayList<>();
+    private ArrayList<String> flat_number = new ArrayList<>();
     private Integer clientID;
 
     public ClientDataDummy(){
         clientID = 0;
 
         cities.add("Warsaw");
-        cities.add("Cracow");
 
-        streets.add("Jana");
-        streets.add("Kazimierza");
-        streets.add("Pawla");
+        streets.add("Jana Pawla 2");
+
+        building_number.add("22");
+
+        flat_number.add("1");
 
         post_code.add("00-001");
-        post_code.add("01-010");
-        post_code.add("10-100");
     }
 
     public ClientData getClient(){
@@ -34,7 +35,9 @@ public class ClientDataDummy {
         clientAddress.setCity(cities.get(clientID % cities.size()));
         clientAddress.setPostCode(post_code.get(clientID % post_code.size()));
         clientAddress.setStreet(streets.get(clientID % streets.size()));
-
+        clientAddress.setBuildingNumber(building_number.get(clientID % building_number.size()));
+        clientAddress.setFlatNumber(flat_number.get(clientID % flat_number.size()));
+        
         clientData.setClientAddresses(clientAddress);
 
         return clientData;
