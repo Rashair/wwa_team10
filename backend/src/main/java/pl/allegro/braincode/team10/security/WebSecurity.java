db7f58427a6cfd22bbcbd89a8215e60312d17a02
kjedrzejczak <jedrek412@gmail.com>
2019-04-13 07:28:47 +0200

    Added spring security in dependencies Added cors config

---
+++ backend/src/main/java/pl/allegro/braincode/team10/security/WebSecurity.java
@@ -0,0 +1,33 @@
+package pl.allegro.braincode.team10.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
+
+@EnableWebSecurity
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable().authorizeRequests()
+                .anyRequest().permitAll();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        List<String> allowedMethods = Arrays.asList("DELETE", "GET", "OPTIONS", "POST", "PUT");
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.applyPermitDefaultValues();
+        corsConfig.setAllowedMethods(allowedMethods);
+        source.registerCorsConfiguration("/**", corsConfig);
+        return source;
+    }
+}
