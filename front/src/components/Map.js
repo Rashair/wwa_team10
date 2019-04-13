d7efdc7562860597d299072c89f2777241945d13
Jan Barszcz <jan@MacBook-Pro-Jan.local>
2019-04-13 06:36:16 +0200

    fixed marker rendering after fetch

--- front/src/components/Map.js
+++ front/src/components/Map.js
@@ -3,6 +3,14 @@ import { withGoogleMap, GoogleMap, Marker, InfoWindow } from 'react-google-maps'
 import PropTypes from 'prop-types'
 import Markers from './Markers.js'
 
+const GoogleMapRendered = withGoogleMap(props => (
+    <GoogleMap
+        defaultCenter={{ lat: 52.2297, lng: 21.0122 }}
+        defaultZoom={6}
+    >
+        {props.children}
+    </GoogleMap>
+));
 
 class Map extends Component {
 
@@ -16,23 +24,14 @@ class Map extends Component {
     }
 
     render() {
-
-        const GoogleMapRendered = withGoogleMap(props => (
-            <GoogleMap
-                defaultCenter={{ lat: 52.2297, lng: 21.0122 }}
-                defaultZoom={6}
-            >
-                <Markers markersData={this.props.markersData} />
-            </GoogleMap>
-        ));
-
-
         return (
             <div>
                 <GoogleMapRendered
                     containerElement={<div style={{ height: `100%`, width: '100%' }} />}
                     mapElement={<div style={{ height: `100%` }} />}
-                />
+                >
+                    <Markers markersData={this.props.markersData} />
+                </GoogleMapRendered>
             </div>
         );
     }
